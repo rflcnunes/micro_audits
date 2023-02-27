6055fa0172d4e334c68d9470a4d60b26433ae28b
rafael <rafaelcnunes@icloud.com>
2023-02-26 21:41:04 -0300

    added hot reload in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.APP_LISTEN_PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
